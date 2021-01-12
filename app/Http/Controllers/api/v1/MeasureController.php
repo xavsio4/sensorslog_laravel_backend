930687342eb5cfe0cbd48790aa7e9e7288357f4b
xavier Villamuera <gzav@fifteenpeas.com>
2021-01-12 14:00:13 +0100

    - Get measures date descending by default

--- app/Http/Controllers/api/v1/MeasureController.php
+++ app/Http/Controllers/api/v1/MeasureController.php
@@ -31,7 +31,7 @@ class MeasureController extends Controller
     {
         $user = $this->guard()->user();
         
-        $measures = Measure::where('user_id',$user->id)->get();
+        $measures = Measure::where('user_id',$user->id)->orderBy('created_at','desc')->get();
         // $measures = Measure::all();
         return response()->json([
         'success' => true,
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,55 +3,57 @@
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\CanResetPassword;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Passport\HasApiTokens;
 
+
 class User extends Authenticatable
 {
     use HasFactory, HasApiTokens, Notifiable;
-
+    
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+    'name',
+    'email',
+    'password',
     ];
-
+    
     /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+    * The attributes that should be hidden for arrays.
+    *
+    * @var array
+    */
     protected $hidden = [
-        'password',
-        'remember_token',
+    'password',
+    'remember_token',
     ];
-
+    
     /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
+    * The attributes that should be cast to native types.
+    *
+    * @var array
+    */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+    'email_verified_at' => 'datetime',
     ];
-
-     public function accessTokens()
+    
+    public function accessTokens()
     {
         return $this->hasMany('App\Models\OauthAccessToken');
     }
-
-     public function measures()
+    
+    public function measures()
     {
         return $this->hasMany('App\Models\Measure');
     }
-
+    
     public function apikeys()
     {
         return $this->hasMany('App\Models\Apikey');
