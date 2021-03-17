a69623d5ff802454fd50fdb1f4a2f447273a1f69
xavier Villamuera <gzav@fifteenpeas.com>
2021-03-17 10:38:44 +0100

    -bugfix

--- app/Http/Controllers/api/v1/MeasureController.php
+++ app/Http/Controllers/api/v1/MeasureController.php
@@ -192,7 +192,7 @@ class MeasureController extends Controller
         //DB::select('SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,\'ONLY_FULL_GROUP_BY\',\'\'))');
         $measures = DB::select('select max(id),measure_type,origin,measure_unit, max(created_at),max(measure_value) from `measures`
         where user_id = :id
-        group by measure_type,origin', ['id' => $user->id]);
+        group by measure_type,origin,measure_unit', ['id' => $user->id]);
         return response()->json([
         'success' => true,
         'data' => $measures
